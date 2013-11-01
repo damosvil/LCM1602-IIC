76490a6d8bddfb1f9fdec740093f47d90db08851
Daniel Mosquera <milkatoffeecuco@gmail.com>
2013-11-01 15:48:02 -0700

    Fixed a bug

--- main.c
+++ main.c
@@ -10,11 +10,10 @@ int main(int argc, const char *argv[])
 		printf("Ejemplo: LCD /dev/i2c-0 0x20\r\n");
 		return -1;
 	}
+	printf("Using device %s 0x%x\r\n", argv[1], atoi(argv[2]));
 
 	int file = initialize(argv[1], atoi(argv[2]));
 	locate(file, 1, 0);
 	print(file, "Hola mundo 2");
 	finalize();
-
-	printf("Used device %s 0x%x\r\n", argv[1], atoi(argv[2]));
 }
